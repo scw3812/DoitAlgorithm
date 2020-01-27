b1f0f5c961c6cd6b500795d35116bdea05a473f9
scw3812 <dlffpraos@naver.com>
2020-01-27 22:10:58 +0900

    ch5

--- DoItAlgorithm/hanoi.c
+++ DoItAlgorithm/hanoi.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 
 void move(int no, int x, int y) {
+	char name[] = { 'A', 'B', 'C' };
 	if (no > 1) {
-		move(no - 1, x, 6- x - y);
+		move(no - 1, x, 6 - x - y);
 	}
-	printf("원반[%d]를(을) %d 기둥에서 %d 기둥으로 옮김\n", no, x, y);
+	printf("원반[%d]를(을) %c 기둥에서 %c 기둥으로 옮김\n", no, name[x - 1], name[y - 1]);
 	if (no > 1) {
 		move(no - 1, 6 - x - y, y);
 	}
 }
 
-int main5_4(void) {
+int main(void) {
 	int n;
 	printf("하노이의 탑 \n원반 개수 : ");
 	scanf_s("%d", &n);
--- DoItAlgorithm/recur.c
+++ DoItAlgorithm/recur.c
@@ -31,30 +31,40 @@ Top :
 }
 
 void recur3(int n) {
-	IntStack s;
-	Initialize(&s, 100);
+	int x = 0;
+	IntStack s1, s2;
+	Initialize(&s1, 100);
+	Initialize(&s2, 100);
 
-Top:
-	if (n > 0) {
-		Push(&s, n);
-		n = n - 1;
-		goto Top;
-	}
-	if (n > 1) {
-		Push(&s, n);
-		n = n - 2;
-		goto Top;
-	}
-	if (!IsEmpty(&s)) {
-		Pop(&s, &n);
-		printf("%d\n", n);
-		goto Top;
-	}
+	while (1) {
+		if (n > 0) {
+			Push(&s1, n);
+			Push(&s2, x);
 
-	Terminate(&s);
+			if (x == 0) {
+				n = n - 1;
+			}
+			else if(x == 1){
+				n = n - 2;
+				x = 0;
+			}
+			continue;
+		}
+		do {
+			Pop(&s1, &n);
+			Pop(&s2, &x);
+			x++;
+			if (x == 2) {
+				printf("%d\n", n);
+				if (IsEmpty(&s1)) return;
+			}
+		} while (x == 2);
+	}
+	Terminate(&s1);
+	Terminate(&s2);
 }
 
-int main(void) {
+int main5_3(void) {
 	int x;
 	printf("정수를 입력하세요 : ");
 	scanf_s("%d", &x);
